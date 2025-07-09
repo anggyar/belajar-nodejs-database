e7804f704a24c0131f40e6453ca04f8767094dd2
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-07-09 15:53:24 +0700

    Add new customer model, and use jest for testing CRUD operation with Prisma ORM

--- database.sql
+++ database.sql
@@ -4,3 +4,15 @@ CREATE Table sample (
     PRIMARY KEY (id)
 ) ENGINE = InnoDB;
 
+
+CREATE TABLE customers (
+    id VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    constraint customer_email_unique UNIQUE (email),
+    constraint customer_phone_unique UNIQUE (phone)
+) ENGINE = InnoDB;
+
+SELECT * FROM customers;
---
+++ test/crud.test.js
@@ -0,0 +1,61 @@
+import { prismaClient } from "../src/prisma-client";
+
+describe("Prisma Clinet", () => {
+    it("Should be able to create customer", async () => {
+        const customer = await prismaClient.customer.create({
+            data: {
+                id: "anggyar",
+                email: "anggyar@gmail.com",
+                name: "Anggyar Muhamad Yahya",
+                phone: "082188348808",
+            },
+        });
+
+        expect(customer.id).toBe("anggyar");
+        expect(customer.email).toBe("anggyar@gmail.com");
+        expect(customer.name).toBe("Anggyar Muhamad Yahya");
+        expect(customer.phone).toBe("082188348808");
+    });
+
+    it("Should be able to update customer", async () => {
+        const customer = await prismaClient.customer.update({
+            data: {
+                name: "Anggyar Ganteng",
+            },
+            where: {
+                id: "anggyar",
+            },
+        });
+
+        expect(customer.id).toBe("anggyar");
+        expect(customer.email).toBe("anggyar@gmail.com");
+        expect(customer.name).toBe("Anggyar Ganteng");
+        expect(customer.phone).toBe("082188348808");
+    });
+
+    it("Should be able to read customer", async () => {
+        const customer = await prismaClient.customer.findUnique({
+            where: {
+                id: "anggyar",
+            },
+        });
+
+        expect(customer.id).toBe("anggyar");
+        expect(customer.email).toBe("anggyar@gmail.com");
+        expect(customer.name).toBe("Anggyar Ganteng");
+        expect(customer.phone).toBe("082188348808");
+    });
+
+    it("Should be able to delete customer", async () => {
+        const customer = await prismaClient.customer.delete({
+            where: {
+                id: "anggyar",
+            },
+        });
+
+        expect(customer.id).toBe("anggyar");
+        expect(customer.email).toBe("anggyar@gmail.com");
+        expect(customer.name).toBe("Anggyar Ganteng");
+        expect(customer.phone).toBe("082188348808");
+    });
+});
