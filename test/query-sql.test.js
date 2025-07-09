04d7386d00169d41c660233e807b5730ced31915
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-07-09 15:11:43 +0700

    Learn: Use $queryRaw for selecting data on database

---
+++ test/query-sql.test.js
@@ -0,0 +1,16 @@
+import { prismaClient } from "../src/prisma-client";
+
+describe("Prisma Client", () => {
+    it("Should be abel to execute sql", async () => {
+        const id = 1;
+
+        const samples =
+            await prismaClient.$queryRaw`SELECT * FROM sample WHERE id = ${id}`;
+
+        for (const sample of samples) {
+            console.info(
+                `Result sample id : ${sample.id} and name ${sample.name}`
+            );
+        }
+    });
+});
